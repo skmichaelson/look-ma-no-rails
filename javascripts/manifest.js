5881290a834ceb41bb2ee7861964ec6f73648bdb
Sarah Michaelson <sarah.michaelson@gmail.com>
2014-03-27 19:43:49 -0400

    Install event handlers that log the event target

--- javascripts/manifest.js
+++ javascripts/manifest.js
@@ -1,4 +1,20 @@
 
 $(document).ready(function() {
 	console.log("Everything successfully loaded!");
+	$('#about_me').on('click', function () {
+		console.log('clicked on about_me')
+	});
+	
+	$('#projects').on('click', function () {
+		console.log('clicked on projects')
+	});
+	
+	$('#contact').on('click', function () {
+		console.log('clicked on contact')
+	});
+	
+	$('.resume').on('click', function () {
+		console.log('clicked on resume')
+	});
+
 });
